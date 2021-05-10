de1b4ef058726768c6017d285590b1f5d9dc1865
molly-yu <55213953+molly-yu@users.noreply.github.com>
2021-05-09 23:33:12 -0400

    clean up code

--- src/components/nominations.jsx
+++ src/components/nominations.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import styled from "styled-components";
 
 import OpenIcon from '../assets/open.png';
 import Button from './button';
--- src/components/searchbar.jsx
+++ src/components/searchbar.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import InputBar from './inputbar';
 
 const SearchBar = ({keyword, setKeyword, searchEnter}) => {
-    //const BarStyling = {width: "calc(100% - 91px)",background:"#F2F1F9", border: "1px solid #D8D5D5", padding:"0.5rem"};
+
     return (
       <InputBar
        key=""
